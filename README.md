f717903abd670e9d5ff5d0116fa4f28a401cc95c
Vincent <cottalorda.vincent@gmail.com>
2022-02-11 12:45:50 +0100

    V1.0

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # symfony-template
+
+## Contient :
+
+1. Squelette d'application Symfony
+2. Un env-sample de base
+3. plusieurs packages dont :
+
+-   Annotations
+-   Twig (si besoin de gérer le front directement)
+-   Security (pour l'authentification)
+-   Maker (pour générer facilemnt des fichier depuis la cli)
+-   Form (pour générer des formulaire à inserer dans twig)
+-   Doctrine (l'ORM de symfony 🔥)
+-   Debug (pour debbugger)
+-   Api Platform (pour générer une api a la route /api)
+
+## pour lancer
+
+-   `composer update`
+
+Pour installer les dépendances
+
+-   `symfony serve`
+
+Pour lancer le serveur symfony
+
+-   rdv a l'url local suivi de /api pour voir la documentation généré par api platform
+
+-   a vous de jouer :)
---
+++ src/Entity/Example.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ExampleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExampleRepository::class)]
+#[ApiResource]
+class Example
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'integer')]
+    private $nombre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNombre(): ?int
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(int $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+}
